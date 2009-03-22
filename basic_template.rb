4c0b872a2e1350eec9329cb10fca703f05ee0f91
Mathias Stjernström <mathias@globalinn.com>
2009-03-22 14:37:50 +0100

    Tuning the scripts :)

--- basic_template.rb
+++ basic_template.rb
@@ -1,3 +1,4 @@
+
 run "echo FIXME > README"
 
 git :init
@@ -13,12 +14,9 @@ END
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
 run "cp config/database.yml config/example_database.yml"
 
-git :add => ".", :commit => "-m 'initial commit'"
-
 generate :controller, "welcome"
 route "map.root :controller => 'welcome'"
 
 git :rm => "public/index.html"
 
-git :add => ".", :commit => "-m 'adding welcome controller'"
-
+git :add => ".", :commit => "-m 'initial commit'"
--- nifty_template.rb
+++ nifty_template.rb
@@ -1,7 +1,7 @@
 load_template "http://github.com/stjernstrom/rails_templates/raw/master/basic_template.rb"
 # load_template "/Users/mathias/Documents/Dev/Public/rails_templates/basic_template.rb"
 
-gem "nifty-generators"
+#gem "nifty-generators"
 
 generate :nifty_layout
 generate :nifty_config
@@ -21,3 +21,5 @@ file 'app/views/welcome/index.html.erb', <<-CODE
 <% title "Welcome!" %>
 <p>Hello and welcome!</p>
 CODE
+
+git :add => ".", :commit => "-m 'niftyfied project with nifty_template'"
